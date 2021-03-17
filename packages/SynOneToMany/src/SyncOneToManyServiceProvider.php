ea377c084f1c6fd52357bbb06701a3b808cf507e
nicollasmatheus37 <nicollasmatheus37@gmail.com>
2021-03-17 08:06:24 -0300

    add package folder

---
+++ packages/SynOneToMany/src/SyncOneToManyServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SyncOneToMany;
+
+use Illuminate\Support\ServiceProvider;
+
+class SyncOneToManyServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
